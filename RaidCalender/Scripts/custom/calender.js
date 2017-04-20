1afcadb75cef8a5b25ca4a544aea00e23f2a615a
dragoono <niklas.grieger1@gmail.com>
2017-04-20 21:05:45 +0200

    Form: Details und create to calender - angular - timepicker

--- RaidCalender/Scripts/custom/calender.js
+++ RaidCalender/Scripts/custom/calender.js
@@ -1,19 +1,54 @@
 ﻿$(document).ready(function () {
+    //var eventApp = angular.module('event', []);
+    //eventApp.controller('EventController', ['$scope', function ($scope) {
+    //    $scope.master = {};
+    //        console.log("im angular controller");
+    //    $scope.update = function (event) {
+    //        console.log(event);
+    //        $scope.master = angular.copy(master);
+    //    };
+
+    //    $scope.reset = function () {
+    //        $scope.user = angular.copy($scope.master);
+    //    };
+
+    //    $scope.reset();
+    //}]);
+    //$('.timepicker').timepicker({
+    //    timeFormat: 'HH:mm',
+    //    interval: 60,
+    //    minTime: '00:00',
+    //    maxTime: '23:00',
+    //    defaultTime: '00:00',
+    //    startTime: '00:00',
+    //    dynamic: true,
+    //    dropdown: true,
+    //    scrollbar: true
+    //});
+    //http://jonthornton.github.io/jquery-timepicker/
     $('.timepicker').timepicker({
-        timeFormat: 'HH:mm',
-        interval: 60,
-        minTime: '00:00',
-        maxTime: '23:00',
-        defaultTime: '00:00',
-        startTime: '00:00',
-        dynamic: true,
-        dropdown: true,
-        scrollbar: true
+        //Set the scroll position to local time if no value selected.
+        'scrollDefault': 'now',
+        //timepicker.jquery uses the time portion of PHP's date formatting commands. (http://php.net/manual/en/function.date.php)
+        'timeFormat': 'H:i',
+        //jquery-timepicker allows entering times via the keyboard. 
+        //Setting forceRoundTime to true will round the entered time to the nearest option on the dropdown list.
+        'forceRoundTime': true,
+        'disableTextInput':true,
+        'orientation': 'b',
+        //Update the input with the currently highlighted time value when the timepicker loses focus.
+        'selectOnBlur':true
     });
-    //The Timepicker was on z-index: auto, this is to prevent this
-    $("#cvon").closest('div').click(function () {
-            $(".ui-timepicker-container.ui-timepicker-standard").css("z-index", "7");
-    });
+    $('#cvon').timepicker('setTime', new Date());
+    $('#cbis').timepicker('setTime', new Date());
+    //The Timepicker was on z-index: auto (doesnt show), this is to prevent this
+    //$("#cvon").closest('div').click(function () {
+    //        $(".ui-timepicker-wrapper").css("z-index", "7");
+    //});
+    ////The Timepicker was on z-index: auto (doesnt show), this is to prevent this
+    //$("#cbis").closest('div').click(function () {
+    //    $(".ui-timepicker-container.ui-timepicker-standard").css("z-index", "7");
+    //});
     $(".close").click(function () {
         console.log("click close");
         $("#createEvent").css("display", "none");
