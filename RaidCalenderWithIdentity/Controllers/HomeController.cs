57871d612ac774e01b88215a2baf556cc03ce207
dragoono <niklas.grieger1@gmail.com>
2017-04-28 09:14:49 +0200

    Update HomeController.cs

--- RaidCalenderWithIdentity/Controllers/HomeController.cs
+++ RaidCalenderWithIdentity/Controllers/HomeController.cs
@@ -9,17 +9,23 @@ namespace RaidCalenderWithIdentity.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// The database
+        /// </summary>
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             ViewBag.Username = User.Identity.Name;
             return View(from ApplicationUserManager.Rassen );
         }
-        public String GetData()
+        public JsonResult GetData()
         {
-            RaidCalenderRepository repo = new RaidCalenderRepository();
-            
-                return repo.GetData();
-            
+            return Json((from a in db.Users
+                         select new
+                         {
+                             user = a.Id
+                         }), JsonRequestBehavior.AllowGet);
+
         }
     }
 }
