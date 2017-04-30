0ebd77475f4240ac50b2f6b56aab86b1af7e689b
dragoono <niklas.grieger1@gmail.com>
2017-04-30 04:35:39 +0200

    Pfadänderungen

---
+++ RaidCalenderWithIdentity/Controllers/Gw2RaidCalenderController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNet.Identity;
+using RaidCalenderWithIdentity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RaidCalenderWithIdentity.Controllers
+{
+    public class Gw2RaidCalenderController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Gw2RaidCalender
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllUser()
+        {
+
+            return Json(_db.Users.ToList(), JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult GetUserCharsByCurrentUser()
+        {
+
+            var list = _db.Database.SqlQuery<ViewUserCharsModel>("Select * from dbo.UserChars where UserName = @p0", User.Identity.Name);
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        public void AddNewCharToUser(int klasse_id, int rasse_id, string bezeichnung)
+        {
+
+            var klasse2User = new Klasse2User
+            {
+                User_Id = User.Identity.GetUserId<int>(),
+                Klasse_Id = klasse_id,
+                Rasse_Id = rasse_id,
+                Bezeichnung = bezeichnung
+            };
+            if (klasse2User.Klasse2User_Id == 0)
+            {
+                _db.Entry(klasse2User).State = EntityState.Added;
+
+            }
+            else
+            {
+                _db.Entry(klasse2User).State = EntityState.Modified;
+            }
+            try
+            {
+                _db.Klasse2User.Add(klasse2User);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        public JsonResult GetAllClasses()
+        {
+            var list = _db.Database.SqlQuery<KlasseModel>("Select * from dbo.Klasse");
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetAllRace()
+        {
+            var list = _db.Database.SqlQuery<RasseModel>("Select * from dbo.Rasse");
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        public void DeleteChar(int? id)
+        {
+            if (id != null)
+            {
+                var row = new Klasse2User { Klasse2User_Id = (int)id };
+                _db.Klasse2User.Attach(row);
+                _db.Klasse2User.Remove(row);
+                _db.SaveChanges();
+            }
+            else
+            {
+                var latestRow = _db.Klasse2User.OrderByDescending(p => p.Klasse2User_Id).FirstOrDefault();
+                var newId = latestRow.Klasse2User_Id + 1;
+                _db.Klasse2User.Remove(latestRow);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
